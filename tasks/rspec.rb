8d0361adc5db29fb651d4debd18d1255f4078a15
Eric Watson <wasnotrice@gmail.com>
2015-01-06 12:49:59 -0600

    WIP: Rspec

--- tasks/rspec.rb
+++ tasks/rspec.rb
@@ -1,4 +1,6 @@
 require 'opal'
+require 'opal/rspec'
+require 'opal/rspec/rake_task'
 require_relative 'workaround_for_using_prerelease_gems'
 require_relative 'workaround_for_binary_encoding'
 
@@ -11,8 +13,10 @@ Opal.use_gem 'shoes-core'
 Opal.use_gem 'rspec-its'
 #Opal.use_gem 'webmock'
 
-require 'opal/rspec/rake_task'
-Opal::RSpec::RakeTask.new('spec:dsl')
+Opal::RSpec::RakeTask.new('spec:dsl') do |server|
+  server.debug = true
+end
+
 Opal::RSpec::RakeTask.new('spec:browser') do |server|
   server.debug = true
   server.index_path = File.expand_path('../../spec/browser/index.erb', __FILE__)
